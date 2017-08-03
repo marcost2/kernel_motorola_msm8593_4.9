c07ca8a310e7a3c85eec0b0b5d6e30f0cb4d6e6b
Weilong Wu <wuwl10@motorola.com>
2017-08-03 17:01:59 +0800

    Revert "NFC: Inform CLF whenever eSE is powered off"
    This reverts commit 05e6affbcc6966ba15229fdf6b9a04728948b923.
    
    Mot-CRs-fixed: (CR)
    
    Don't need this logic as our product don't have ese.
    
    Change-Id: Ib68a9db4a284780d3cabc695c6dd332c92f35088
    Signed-off-by: Weilong Wu <wuwl10@motorola.com>
    Reviewed-on: https://gerrit.mot.com/1035855
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Balachandar Swami <balaswami@motorola.com>
    Reviewed-by: Mingyong Lu <lumy1@motorola.com>
    Reviewed-by: Huosheng Liao <liaohs@motorola.com>
    Submit-Approved: Jira Key

--- drivers/nfc/nq-nci.h
+++ drivers/nfc/nq-nci.h
@@ -50,7 +50,6 @@ enum nfcc_chip_variant {
 	NFCC_NQ_330			= 0x51,	/**< NFCC NQ330 */
 	NFCC_SN100_A			= 0xa3,	/**< NFCC SN100_A */
 	NFCC_SN100_B			= 0xa4,	/**< NFCC SN100_B */
-	NFCC_PN66T			= 0x18,	/**< NFCC PN66T */
 	NFCC_NOT_SUPPORTED	        = 0xFF	/**< NFCC is not supported */
 };
 #endif
