5172abd115aec8b4f2dc24e13c11f63419ee3a22
wiktorek140 <wiktorek140@gmail.com>
2021-03-11 10:28:29 +0100

    techpack: q6asm: replace ifdefs with more common config
    Signed-off-by: wiktorek140 <wiktorek140@gmail.com>
    Change-Id: I7cfafb0a91b739a4283f4a22cc67c58b1673f6d9

--- techpack/audio/dsp/q6asm.c
+++ techpack/audio/dsp/q6asm.c
@@ -2987,7 +2987,7 @@ EXPORT_SYMBOL(q6asm_open_read_v3);
 int q6asm_open_read_v4(struct audio_client *ac, uint32_t format,
 			uint16_t bits_per_sample, bool ts_mode)
 {
-#if CONFIG_MONTANA_DTB
+#ifdef CONFIG_SND_LEGACY
 	return q6asm_open_read_v3(ac, format, bits_per_sample);
 #endif
 	return __q6asm_open_read(ac, format, bits_per_sample,
@@ -3329,7 +3329,7 @@ EXPORT_SYMBOL(q6asm_open_write_v3);
 int q6asm_open_write_v4(struct audio_client *ac, uint32_t format,
 			uint16_t bits_per_sample)
 {
-#if CONFIG_MONTANA_DTB
+#ifdef CONFIG_SND_LEGACY
 	return q6asm_open_write_v3(ac, format, bits_per_sample);
 #endif
 	return __q6asm_open_write(ac, format, bits_per_sample,
@@ -3396,7 +3396,7 @@ int q6asm_stream_open_write_v4(struct audio_client *ac, uint32_t format,
 			       uint16_t bits_per_sample, int32_t stream_id,
 			       bool is_gapless_mode)
 {
-#if CONFIG_MONTANA_DTB
+#ifdef CONFIG_SND_LEGACY
 	return q6asm_stream_open_write_v3(ac, format, bits_per_sample,
 						stream_id, is_gapless_mode);
 #endif
@@ -4714,7 +4714,7 @@ int q6asm_enc_cfg_blk_pcm_v4(struct audio_client *ac,
 	u32 frames_per_buf = 0;
 	int rc;
 
-#if CONFIG_MONTANA_DTB
+#ifdef CONFIG_SND_LEGACY
 	return q6asm_enc_cfg_blk_pcm_v3(ac, rate, channels, bits_per_sample,
 					use_default_chmap, use_back_flavor,
 					channel_map, sample_word_size);
@@ -5021,6 +5021,11 @@ static int __q6asm_enc_cfg_blk_pcm_v4(struct audio_client *ac,
 				      uint16_t endianness,
 				      uint16_t mode)
 {
+#ifdef CONFIG_SND_LEGACY
+	return q6asm_enc_cfg_blk_pcm_v3(ac, rate, channels,
+					bits_per_sample, true, false, NULL,
+					sample_word_size);
+#endif
 	return q6asm_enc_cfg_blk_pcm_v4(ac, rate, channels,
 					bits_per_sample, true, false, NULL,
 					sample_word_size, endianness, mode);
@@ -5105,6 +5110,11 @@ int q6asm_enc_cfg_blk_pcm_format_support_v4(struct audio_client *ac,
 					    uint16_t endianness,
 					    uint16_t mode)
 {
+#ifdef CONFIG_SOC_LEGACY
+	return q6asm_enc_cfg_blk_pcm_format_support_v3(ac, rate,
+						channels, bits_per_sample, sample_word_size);
+#endif
+
 	return __q6asm_enc_cfg_blk_pcm_v4(ac, rate, channels,
 					   bits_per_sample, sample_word_size,
 					   endianness, mode);
@@ -6248,7 +6258,7 @@ int q6asm_media_format_block_pcm_format_support_v4(struct audio_client *ac,
 						   uint16_t endianness,
 						   uint16_t mode)
 {
-#if CONFIG_MONTANA_DTB
+#ifdef CONFIG_SND_LEGACY
 	return q6asm_media_format_block_pcm_format_support_v3(ac, rate, channels,
 				bits_per_sample, stream_id,
 				use_default_chmap, channel_map,
@@ -6678,7 +6688,7 @@ int q6asm_media_format_block_multi_ch_pcm_v4(struct audio_client *ac,
 					     uint16_t endianness,
 					     uint16_t mode)
 {
-#if CONFIG_MONTANA_DTB
+#ifdef CONFIG_SND_LEGACY
 	return q6asm_media_format_block_multi_ch_pcm_v3(ac, rate, channels,
 				use_default_chmap, channel_map,
 				bits_per_sample, sample_word_size);
