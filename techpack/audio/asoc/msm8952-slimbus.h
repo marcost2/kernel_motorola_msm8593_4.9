MotorolaMobilityLLC/kernel-msm

ASoC: msm8952: Add cs35l34 amp to machine driver.
Add the cs35l34 amp to the machine driver with marley as the
master to the amp.

Change-Id: Ic6a8395239702f933f27aa572e948b98990c4e71
Signed-off-by: Paul Handrigan <Paul.Handrigan@cirrus.com>
Mot-CRs-fixed: (CR): Addison Marley audio codec bringup
Reviewed-on: http://gerrit.mot.com/827735
SME-Granted: SME Approvals Granted
SLTApproved: Slta Waiver <sltawvr@motorola.com>
Tested-by: Jira Key <jirakey@motorola.com>
Reviewed-by: Rajasekhar Ram Goparaju <chb764@motorola.com>
Reviewed-by: Igor Kovalenko <igork@motorola.com>
Submit-Approved: Jira Key <jirakey@motorola.com>

sound/soc/msm/msm8952-slimbus.h
@@ -57,6 +57,7 @@ int msm_proxy_tx_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
 int msm_audrx_init(struct snd_soc_pcm_runtime *rtd);
 #ifdef CONFIG_SND_SOC_MARLEY
 int marley_dai_init(struct snd_soc_pcm_runtime *rtd);
+int marley_cs35l34_dai_init(struct snd_soc_pcm_runtime *rtd);
 #endif
 int msm_mi2s_snd_hw_params(struct snd_pcm_substream *substream,
 			     struct snd_pcm_hw_params *params);

marcost2/kernel_motorola_msm8593_4.9

techpack/audio/asoc/msm8952-slimbus.h
@@ -91,6 +91,7 @@ int msm_tdm_be_hw_params_fixup(struct snd_soc_pcm_runtime *rtd,
 int msm_audrx_init(struct snd_soc_pcm_runtime *rtd);
 #ifdef CONFIG_SND_SOC_MADERA
 int madera_dai_init(struct snd_soc_pcm_runtime *rtd);
+int madera_cs35l34_dai_init(struct snd_soc_pcm_runtime *rtd);
 #endif
 int msm_mi2s_snd_hw_params(struct snd_pcm_substream *substream,
 			     struct snd_pcm_hw_params *params);