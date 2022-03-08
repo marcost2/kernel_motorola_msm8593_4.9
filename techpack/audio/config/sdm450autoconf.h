3bde6822c2ea702076d38b9a530fbed957e87f00
marcost2 <marcostolcachir@gmail.com>
2022-03-08 17:24:41 -0300

    techpack: audio: Enable and add sdm450 config

--- techpack/audio/config/sdm450autoconf.h
+++ techpack/audio/config/sdm450autoconf.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, 2020, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2018, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -17,8 +17,8 @@
 #define CONFIG_SND_SOC_WCD_CPE 1
 #define CONFIG_SND_SOC_CPE 1
 #define CONFIG_SND_SOC_WCD_MBHC 1
-#define CONFIG_SND_SOC_WSA881X 1
-#define CONFIG_SND_SOC_WSA881X_ANALOG 1
+//#define CONFIG_SND_SOC_WSA881X 1
+//#define CONFIG_SND_SOC_WSA881X_ANALOG 1
 #define CONFIG_SND_SOC_WCD9335 1
 #define CONFIG_MSM_QDSP6V2_CODECS 1
 #define CONFIG_MSM_ULTRASOUND 1
@@ -28,7 +28,6 @@
 #define CONFIG_MSM_QDSP6_SSR 1
 #define CONFIG_MSM_QDSP6_NOTIFIER 1
 #define CONFIG_SND_SOC_MSM_HOSTLESS_PCM 1
-#define CONFIG_SND_SOC_MSM_QDSP6V2_INTF 1
 #define CONFIG_SND_SOC_SDM450 1
 #define CONFIG_SND_SOC_EXT_CODEC_SDM450 1
 #define CONFIG_SOUNDWIRE 1
@@ -46,6 +45,8 @@
 #define CONFIG_WCD9XXX_CODEC_CORE 1
 #define CONFIG_MSM_CDC_PINCTRL 1
 #define CONFIG_SND_SOC_MSM_STUB 1
+#define CONFIG_MSM_AVTIMER 1
+// #define CONFIG_AVTIMER_LEGACY 1
 #define CONFIG_SND_SOC_EXT_CODEC 1
 #define CONFIG_SND_SOC_INT_CODEC 1
 #define CONFIG_SND_SOC_SDM660_CDC 1
@@ -54,3 +55,15 @@
 #define CONFIG_SND_SOC_MSM_HDMI_CODEC_RX 1
 #define CONFIG_COMMON_CLK 1
 #define CONFIG_WCD_DSP_GLINK 1
+#define CONFIG_MFD_MADERA 1
+#define CONFIG_MFD_MADERA_SPI 1
+#define CONFIG_SND_SOC_MADERA 1
+#define CONFIG_SND_SOC_MADERA_SLIMBUS 1
+#define CONFIG_SND_SOC_WM_ADSP 1
+#define CONFIG_MFD_CS47L35 1
+#define CONFIG_SND_SOC_CS47L35 1
+#define CONFIG_GPIO_MADERA 1
+#define CONFIG_SND_LEGACY 1
+#define CONFIG_SND_SOC_CS35L35 1
+#define CONFIG_SND_CS35L35_I2C2 1
+#define CONFIG_SND_SOC_OPALUM 1
