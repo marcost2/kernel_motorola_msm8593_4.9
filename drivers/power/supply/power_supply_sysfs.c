375e8181493ef6c8134f16e983ff9c4486090bdd
marcost2 <marcostolcachir@gmail.com>
2023-02-01 18:29:53 -0300

    power: smbcharger-mmi:  Add chg_thermal_mitigation
    * Now if only i could get thermal-engine to not die i might have a thermal system

--- drivers/power/supply/power_supply_sysfs.c
+++ drivers/power/supply/power_supply_sysfs.c
@@ -276,6 +276,7 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(temp_cold),
 	POWER_SUPPLY_ATTR(temp_hot),
 	POWER_SUPPLY_ATTR(system_temp_level),
+	POWER_SUPPLY_ATTR(num_system_temp_levels),
 	POWER_SUPPLY_ATTR(resistance),
 	POWER_SUPPLY_ATTR(resistance_capacitive),
 	POWER_SUPPLY_ATTR(resistance_id),
@@ -360,6 +361,8 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(switch_state),
 	POWER_SUPPLY_ATTR(wakeup),
 	POWER_SUPPLY_ATTR(mask_int),
+	POWER_SUPPLY_ATTR(system_temp_in_level),
+	POWER_SUPPLY_ATTR(num_system_temp_in_levels),
 	/* Local extensions of type int64_t */
 	POWER_SUPPLY_ATTR(charge_counter_ext),
 	/* Properties of type `const char *' */
