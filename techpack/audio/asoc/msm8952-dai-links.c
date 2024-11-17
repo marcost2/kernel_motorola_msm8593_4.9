MotorolaMobilityLLC/kernel-msm

ASoC: msm8953: read tommy cpu_dai_name from devtree
P1B and later will have Tommy at AIF1 but olders at AIF2.
read cpu_dai_name from devtree and link it accordingly.

Change-Id: I2c56e435620be86b124cff053ed5704d4d93e0a6
Signed-off-by: SeungYoon Lee <w22905@motorola.com>
Reviewed-on: https://gerrit.mot.com/910934
SME-Granted: SME Approvals Granted
SLTApproved: Slta Waiver <sltawvr@motorola.com>
Tested-by: Jira Key <jirakey@motorola.com>
Reviewed-by: Igor Kovalenko <igork@motorola.com>
Submit-Approved: Jira Key <jirakey@motorola.com>

sound/soc/msm/msm8952-dai-links.c
@@ -439,7 +439,7 @@ static struct snd_soc_dai_link msm8952_marley_l35_dai_link[] = {
 		.name = "MARLEY-AMP",
 		.stream_name = "MARLEY-AMP Playback",
 		.cpu_name = "marley-codec",
-		.cpu_dai_name = "marley-aif2",
+		.cpu_dai_name = "marley-aif1",
 		.codec_name = "cs35l35.2-0040",
 		.codec_dai_name = "cs35l35-pcm",
 		.init = marley_cs35l35_dai_init,
@@ -1850,7 +1850,8 @@ struct snd_soc_card *populate_snd_card_dailinks(struct device *dev)
 	}
 #ifdef CONFIG_SND_SOC_MARLEY
 	else if (!strncmp(card->name, "msm8952-marley-card", 19)) {
-		int len_2a, len_2b, is_amp_tommy = 0;
+		int ret, len_2a, len_2b, is_amp_tommy = 0;
+		const char *l35_cpu_dai_name;
 
 		if (of_property_read_bool(dev->of_node, "qcom,albus-audio"))
 			is_amp_tommy = 1;
@@ -1877,6 +1878,11 @@ struct snd_soc_card *populate_snd_card_dailinks(struct device *dev)
 			msm8952_marley_be_dai, sizeof(msm8952_marley_be_dai));
 		msm8952_dai_links = msm8952_marley_dai_links;
 		if (is_amp_tommy) {
+			ret = of_property_read_string(dev->of_node,
+				"qcom,l35_cpu_dai_name", &l35_cpu_dai_name);
+			if (ret == 0)
+				msm8952_marley_l35_dai_link[0].cpu_dai_name =
+					l35_cpu_dai_name;
 			memcpy(msm8952_marley_dai_links + len_2a,
 				msm8952_marley_l35_dai_link,
 				sizeof(msm8952_marley_l35_dai_link));

marcost2/kernel_motorola_msm8593_4.9

techpack/audio/asoc/msm8952-dai-links.c
@@ -450,7 +450,7 @@ static struct snd_soc_dai_link msm8952_madera_l35_dai_link[] = {
 		.name = "MADERA-AMP",
 		.stream_name = "MADERA-AMP Playback",
 		.cpu_name = "cs47l35-codec",
-		.cpu_dai_name = "cs47l35-aif2",
+		.cpu_dai_name = "cs47l35-aif1",
 		.codec_name = "cs47l35.2-0040",
 		.codec_dai_name = "cs35l35-pcm",
 		.init = madera_cs35l35_dai_init,
@@ -1867,6 +1867,7 @@ struct snd_soc_card *populate_snd_card_dailinks(struct device *dev)
 	int num_links, ret, len1, len2, len3, len4, len5 = 0;
 #ifdef CONFIG_SND_SOC_MADERA
 	int len_2a, len_2b, is_amp_tommy = 0;
+	const char *l35_cpu_dai_name;
 #endif
 	enum codec_variant codec_ver = 0;
 	const char *tasha_lite[NUM_OF_TASHA_LITE_DEVICE] = {
@@ -1939,6 +1940,11 @@ struct snd_soc_card *populate_snd_card_dailinks(struct device *dev)
 			msm8952_madera_be_dai, sizeof(msm8952_madera_be_dai));
 		msm8952_dai_links = msm8952_madera_dai_links;
 		if (is_amp_tommy) {
+			ret = of_property_read_string(dev->of_node,
+				"qcom,l35_cpu_dai_name", &l35_cpu_dai_name);
+			if (ret == 0)
+				msm8952_madera_l35_dai_link[0].cpu_dai_name =
+					l35_cpu_dai_name;
 			memcpy(msm8952_madera_dai_links + len_2a,
 				msm8952_madera_l35_dai_link,
 				sizeof(msm8952_madera_l35_dai_link));