MotorolaMobilityLLC/kernel-msm

ASoC: msm8953: Set Marley CODEC LDO2 supply to 3.1V
Set the Marley Codec LDO2 supply voltage to 3.1V for setting
the micbias voltages at a higher level.

Change-Id: If84ae286f1deec9889822b70f960f9c9a433789d
Mot-CRs-fixed: (CR)
Signed-off-by: Paul Handrigan <Paul.Handrigan@cirrus.com>
Reviewed-on: https://gerrit.mot.com/947818
SLTApproved: Slta Waiver <sltawvr@motorola.com>
SME-Granted: SME Approvals Granted
Tested-by: Jira Key <jirakey@motorola.com>
Reviewed-by: John Ratke <jratke@motorola.com>
Reviewed-by: Igor Kovalenko <igork@motorola.com>
Submit-Approved: Jira Key <jirakey@motorola.com>

sound/soc/msm/msm8952-slimbus.c
@@ -2996,6 +2996,8 @@ int marley_dai_init(struct snd_soc_pcm_runtime *rtd)
 
 	snd_soc_dapm_sync(dapm);
 
+	/* Set LDO2 to 3.1V */
+	snd_soc_write(codec, ARIZONA_LDO2_CONTROL_1, 0x4A4);
 	/* In current hardware, both Marley MICDETECT and sensor hub
 	 * are connected for test purposes.  This code could be removed
 	 * later.

marcost2/kernel_motorola_msm8593_4.9

techpack/audio/asoc/msm8952-slimbus.c
@@ -4075,6 +4075,9 @@ int madera_dai_init(struct snd_soc_pcm_runtime *rtd)
 	snd_soc_dapm_force_enable_pin(dapm, "SYSCLK");
 	snd_soc_dapm_sync(dapm);
 
+	/* Set LDO2 to 3.1V */
+	snd_soc_write(codec, MADERA_LDO2_CONTROL_1, 0x4A4);
+
 	/* Disable the MCLK */
 	snd_soc_update_bits(codec, MADERA_SYSTEM_CLOCK_1,
 		1 << MADERA_SYSCLK_ENA_SHIFT, 0);