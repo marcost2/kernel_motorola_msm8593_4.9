wiktorek140/msm-4.9

techpack: asoc: routing: add ec port related to QUIN MI2S
Signed-off-by: Wiktor Strzębała <wiktorek140@tlen.pl>
Change-Id: Idc75ed27b44638c68f0a0dbb3ee1d708ba7b8031

techpack/audio/asoc/msm-pcm-routing-v2.c
@@ -3872,6 +3872,16 @@ static int msm_routing_ec_ref_rx_put(struct snd_kcontrol *kcontrol,
 		msm_route_ec_ref_rx = 24;
 		ec_ref_port_id = AFE_PORT_ID_PRIMARY_TDM_RX_1;
 		break;
+#ifdef CONFIG_MONTANA_DTB
+	case 25:
+		msm_route_ec_ref_rx = 25;
+		ec_ref_port_id = AFE_PORT_ID_QUINARY_MI2S_RX;
+		break;
+	case 26:
+		msm_route_ec_ref_rx = 26;
+		ec_ref_port_id = AFE_PORT_ID_QUINARY_MI2S_TX;
+		break;
+#endif
 	default:
 		msm_route_ec_ref_rx = 0; /* NONE */
 		pr_err("%s EC ref rx %ld not valid\n",
@@ -3896,7 +3906,7 @@ static const char *const ec_ref_rx[] = { "None", "SLIM_RX", "I2S_RX",
 	"QUAT_TDM_RX_0", "QUAT_TDM_RX_1", "QUAT_TDM_RX_2", "SLIM_6_RX",
 	"TERT_MI2S_RX", "QUAT_MI2S_RX", "TERT_TDM_TX_0", "USB_AUDIO_RX",
 	"INT0_MI2S_RX", "INT4_MI2S_RX", "INT3_MI2S_TX", "DISPLAY_PORT",
-	"PRI_TDM_RX_1"};
+	"PRI_TDM_RX_1", "QUIN_MI2S_RX", "QUIN_MI2S_TX"};
 
 static const struct soc_enum msm_route_ec_ref_rx_enum[] = {
 	SOC_ENUM_SINGLE_EXT(ARRAY_SIZE(ec_ref_rx), ec_ref_rx),
@@ -3995,6 +4005,9 @@ static int msm_routing_ext_ec_get(struct snd_kcontrol *kcontrol,
 		break;
 	case AFE_PORT_ID_QUATERNARY_MI2S_TX:
 		ucontrol->value.integer.value[0] = EXT_EC_REF_QUAT_MI2S_TX;
+		break;
+	case AFE_PORT_ID_QUINARY_MI2S_TX:
+		ucontrol->value.integer.value[0] = EXT_EC_REF_QUIN_MI2S_TX;
 		break;
 	case SLIMBUS_1_TX:
 		ucontrol->value.integer.value[0] = EXT_EC_REF_SLIM_1_TX;

marcost2/kernel_motorola_msm8593_4.9

techpack/audio/asoc/msm-pcm-routing-v2.c
@@ -3824,6 +3824,16 @@ static int msm_routing_ec_ref_rx_put(struct snd_kcontrol *kcontrol,
 		msm_route_ec_ref_rx = 24;
 		ec_ref_port_id = AFE_PORT_ID_PRIMARY_TDM_RX_1;
 		break;
+#ifdef CONFIG_SND_LEGACY
+	case 25:
+		msm_route_ec_ref_rx = 25;
+		ec_ref_port_id = AFE_PORT_ID_QUINARY_MI2S_RX;
+		break;
+	case 26:
+		msm_route_ec_ref_rx = 26;
+		ec_ref_port_id = AFE_PORT_ID_QUINARY_MI2S_TX;
+		break;
+#endif
 	default:
 		msm_route_ec_ref_rx = 0; /* NONE */
 		pr_err("%s EC ref rx %ld not valid\n",
@@ -3848,7 +3858,7 @@ static const char *const ec_ref_rx[] = { "None", "SLIM_RX", "I2S_RX",
 	"QUAT_TDM_RX_0", "QUAT_TDM_RX_1", "QUAT_TDM_RX_2", "SLIM_6_RX",
 	"TERT_MI2S_RX", "QUAT_MI2S_RX", "TERT_TDM_TX_0", "USB_AUDIO_RX",
 	"INT0_MI2S_RX", "INT4_MI2S_RX", "INT3_MI2S_TX", "DISPLAY_PORT",
-	"PRI_TDM_RX_1"};
+	"PRI_TDM_RX_1", "QUIN_MI2S_RX", "QUIN_MI2S_TX"};
 
 static const struct soc_enum msm_route_ec_ref_rx_enum[] = {
 	SOC_ENUM_SINGLE_EXT(ARRAY_SIZE(ec_ref_rx), ec_ref_rx),
@@ -3947,6 +3957,9 @@ static int msm_routing_ext_ec_get(struct snd_kcontrol *kcontrol,
 		break;
 	case AFE_PORT_ID_QUATERNARY_MI2S_TX:
 		ucontrol->value.integer.value[0] = EXT_EC_REF_QUAT_MI2S_TX;
+		break;
+	case AFE_PORT_ID_QUINARY_MI2S_TX:
+		ucontrol->value.integer.value[0] = EXT_EC_REF_QUIN_MI2S_TX;
 		break;
 	case SLIMBUS_1_TX:
 		ucontrol->value.integer.value[0] = EXT_EC_REF_SLIM_1_TX;