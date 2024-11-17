MotorolaMobilityLLC/kernel-msm

Approach is not working on Android O for Albus
Revert "(CR) Revert "ASoC: msm8953: Add PDM link betweem marley and cs35l35""

This reverts commit 9010566a0290004095422ac3487fb326b123453c.

Original patch from our vendor (Cirrus) is needed to complete
the audio path for ultrasound processing for the Moto Approach
feature.

Change-Id: Ib6421bbc4c0792cb6104391d443164d2fe854314
Reviewed-on: https://gerrit.mot.com/1091677
SME-Granted: SME Approvals Granted
SLTApproved: Slta Waiver
Reviewed-by: Yin-Jun Chen <a7301c@motorola.com>
Tested-by: Jira Key
Submit-Approved: Jira Key

sound/soc/msm/msm8952-dai-links.c
@@ -448,6 +448,14 @@ static const struct snd_soc_pcm_stream cs35l34_params = {
 	.channels_max = 2,
 };
 
+static const struct snd_soc_pcm_stream cs35l35_pdm_params = {
+	.formats = SNDRV_PCM_FMTBIT_S16_LE,
+	.rate_min = 96000,
+	.rate_max = 96000,
+	.channels_min = 1,
+	.channels_max = 2,
+};
+
 static struct snd_soc_dai_link msm8952_marley_l34_dai_link[] = {
 	{
 		.name = "MARLEY-AMP",
@@ -463,6 +471,20 @@ static struct snd_soc_dai_link msm8952_marley_l34_dai_link[] = {
 		.ignore_suspend = 1,
 		.ignore_pmdown_time = 1,
 		.params = &cs35l34_params,
+	},
+	{
+		.name = "MARLEY-PDM",
+		.stream_name = "MARLEY-PDM Playback",
+		.cpu_name = "marley-codec",
+		.cpu_dai_name = "marley-pdm",
+		.codec_name = "cs35l35.2-0040",
+		.codec_dai_name = "cs35l35-pdm",
+		.dai_fmt = SND_SOC_DAIFMT_PDM | SND_SOC_DAIFMT_NB_NF |
+			SND_SOC_DAIFMT_CBS_CFS,
+		.no_pcm = 1,
+		.ignore_suspend = 1,
+		.ignore_pmdown_time = 1,
+		.params = &cs35l35_pdm_params,
 	}
 };
 

marcost2/kernel_motorola_msm8593_4.9

techpack/audio/asoc/msm8952-dai-links.c
@@ -427,6 +427,14 @@ static const struct snd_soc_pcm_stream cs35l34_params = {
 	.channels_max = 2,
 };
 
+static const struct snd_soc_pcm_stream cs35l35_pdm_params = {
+	.formats = SNDRV_PCM_FMTBIT_S16_LE,
+	.rate_min = 96000,
+	.rate_max = 96000,
+	.channels_min = 1,
+	.channels_max = 2,
+};
+
 static struct snd_soc_dai_link msm8952_madera_l34_dai_link[] = {
 	{
 		.name = "MADERA-AMP",
@@ -442,6 +450,20 @@ static struct snd_soc_dai_link msm8952_madera_l34_dai_link[] = {
 		.ignore_suspend = 1,
 		.ignore_pmdown_time = 1,
 		.params = &cs35l34_params,
+	},
+	{
+		.name = "MADERA-PDM",
+		.stream_name = "MADERA-PDM Playback",
+		.cpu_name = "cs47l35-codec",
+		.cpu_dai_name = "cs47l35-pdm",
+		.codec_name = "cs35l35.2-0040",
+		.codec_dai_name = "cs35l35-pdm",
+		.dai_fmt = SND_SOC_DAIFMT_PDM | SND_SOC_DAIFMT_NB_NF |
+			SND_SOC_DAIFMT_CBS_CFS,
+		.no_pcm = 1,
+		.ignore_suspend = 1,
+		.ignore_pmdown_time = 1,
+		.params = &cs35l35_pdm_params,
 	}
 };
 