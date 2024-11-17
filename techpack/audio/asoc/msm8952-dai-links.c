MotorolaMobilityLLC/kernel-msm

ASoC: albus: fix HDMI audio playback
"qcom,dba_cpu_dai_name" should not be used as a condition
to add the dai link to the dai_links array. If present, it
is used to override the default dai's cpu_dai_name.

Change-Id: I0720e5d245170a4ab12d6de6a7e72aea694395ff
Signed-off-by: Vitor Albuquerque <vitoralb@motorola.com>
Reviewed-on: https://gerrit.mot.com/1095798
SLTApproved: Slta Waiver
SME-Granted: SME Approvals Granted
Tested-by: Jira Key
Reviewed-by: Jinjian Wang <e12714@motorola.com>
Reviewed-by: Igor Kovalenko <igork@motorola.com>
Submit-Approved: Jira Key

sound/soc/msm/msm8952-dai-links.c
@@ -2143,22 +2143,22 @@ struct snd_soc_card *populate_snd_card_dailinks(struct device *dev)
 				__func__);
 			ret = of_property_read_string(dev->of_node,
 				"qcom,dba_cpu_dai_name", &dba_cpu_dai_name);
-			if (ret == 0) {
-				if (of_property_read_bool(dev->of_node, "qcom,ali-audio")) {
+			if (of_property_read_bool(dev->of_node, "qcom,ali-audio")) {
+				if (ret == 0)
 					msm8952_ali_hdmi_dba_dai_link[0].cpu_dai_name =
 						dba_cpu_dai_name;
-					memcpy(msm8952_dai_links + len4,
-					       msm8952_ali_hdmi_dba_dai_link,
-					       sizeof(msm8952_ali_hdmi_dba_dai_link));
-					len4 += ARRAY_SIZE(msm8952_ali_hdmi_dba_dai_link);
-				} else {
+				memcpy(msm8952_dai_links + len4,
+						msm8952_ali_hdmi_dba_dai_link,
+						sizeof(msm8952_ali_hdmi_dba_dai_link));
+				len4 += ARRAY_SIZE(msm8952_ali_hdmi_dba_dai_link);
+			} else { /* albus */
+				if (ret == 0)
 					msm8952_albus_hdmi_dba_dai_link[0].cpu_dai_name =
 						dba_cpu_dai_name;
-					memcpy(msm8952_dai_links + len4,
-					       msm8952_albus_hdmi_dba_dai_link,
-					       sizeof(msm8952_albus_hdmi_dba_dai_link));
-					len4 += ARRAY_SIZE(msm8952_albus_hdmi_dba_dai_link);
-				}
+				memcpy(msm8952_dai_links + len4,
+						msm8952_albus_hdmi_dba_dai_link,
+						sizeof(msm8952_albus_hdmi_dba_dai_link));
+				len4 += ARRAY_SIZE(msm8952_albus_hdmi_dba_dai_link);
 			}
 		}
 	} else {

marcost2/kernel_motorola_msm8593_4.9

techpack/audio/asoc/msm8952-dai-links.c
@@ -2116,22 +2116,22 @@ struct snd_soc_card *populate_snd_card_dailinks(struct device *dev)
 				__func__);
 			ret = of_property_read_string(dev->of_node,
 				"qcom,dba_cpu_dai_name", &dba_cpu_dai_name);
-			if (ret == 0) {
-				if (of_property_read_bool(dev->of_node, "qcom,ali-audio")) {
+			if (of_property_read_bool(dev->of_node, "qcom,ali-audio")) {
+				if (ret == 0)
 					msm8952_ali_hdmi_dba_dai_link[0].cpu_dai_name =
 						dba_cpu_dai_name;
-					memcpy(msm8952_dai_links + len5,
-					       msm8952_ali_hdmi_dba_dai_link,
-					       sizeof(msm8952_ali_hdmi_dba_dai_link));
-					len5 += ARRAY_SIZE(msm8952_ali_hdmi_dba_dai_link);
-				} else {
+				memcpy(msm8952_dai_links + len5,
+						msm8952_ali_hdmi_dba_dai_link,
+						sizeof(msm8952_ali_hdmi_dba_dai_link));
+				len5 += ARRAY_SIZE(msm8952_ali_hdmi_dba_dai_link);
+			} else { /* albus */
+				if (ret == 0)
 					msm8952_albus_hdmi_dba_dai_link[0].cpu_dai_name =
 						dba_cpu_dai_name;
-					memcpy(msm8952_dai_links + len5,
-					       msm8952_albus_hdmi_dba_dai_link,
-					       sizeof(msm8952_albus_hdmi_dba_dai_link));
-					len5 += ARRAY_SIZE(msm8952_albus_hdmi_dba_dai_link);
-				}
+				memcpy(msm8952_dai_links + len5,
+						msm8952_albus_hdmi_dba_dai_link,
+						sizeof(msm8952_albus_hdmi_dba_dai_link));
+				len5 += ARRAY_SIZE(msm8952_albus_hdmi_dba_dai_link);
 			}
 		}
 	} else {